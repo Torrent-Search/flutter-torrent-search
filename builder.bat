a8a72861ec8c2ce3d3191942024356c9ae8ccb55
Tejas patil <tejasvp25@gmail.com>
2020-06-16 19:53:18 +0530

    Add builder script

---
+++ builder.bat
@@ -0,0 +1,1 @@
+flutter clean -v &&flutter pub get -v &&flutter packages pub run build_runner clean &&flutter packages pub run build_runner build --delete-conflicting-outputs &&flutter pub run flutter_launcher_icons:main &&flutter build apk --debug --target-platform android-arm64 -v &&flutter build apk --profile --target-platform android-arm64 -v &&flutter build apk --split-per-abi -v
